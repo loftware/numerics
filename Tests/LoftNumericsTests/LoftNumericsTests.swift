3407dbbc84c3f42e460ba71333cc13963e67a474
Sam Lazarus <tyu.sam@gmail.com>
2020-09-22 22:10:19 -0400

    Added type information so that the package at least compiles till it has members

--- Tests/LoftNumericsTests/LoftNumericsTests.swift
+++ Tests/LoftNumericsTests/LoftNumericsTests.swift
@@ -5,5 +5,5 @@ final class LoftNumericsTests: XCTestCase {
     // Add one test per package republished by LoftNumerics to ensure that the
     // public api of the package is being correctly re-exported by LoftNumerics.
 
-    static var allTests = []
+    static var allTests = [(String, () -> Void)]()
 }
